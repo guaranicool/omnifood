a4cd9c8769866515b7feb483b0b9a1d3ad85e366
guaranicool <guarani.cool2030@gmail.com>
2021-12-24 10:20:09 -0300

    added styles: SectionHeroe

--- src/components/SectionHero.vue
+++ src/components/SectionHero.vue
@@ -1,19 +1,28 @@
 <template>
   <section class="section-hero">
-    <!-- text container -->
-    <div class="hero-text-box">
-      <h1 class="heading-primary">
-        A healthy meal delivered to your door, every single day
-      </h1>
-      <p class="hero-desc">
-        The smart 365-days-per-year food subscription that will make you eat
-        healthy again. Tailored to your personal tastes and nutritional needs.
-        We have delivered 250,000+ meals last year!
-      </p>
-    </div>
-    <!-- image container -->
-    <div class="hero-img-box">
-      <img :src="imageBox" alt="hero image" />
+    <div class="hero">
+      <!-- text container -->
+      <div class="hero-text-box">
+        <h1 class="heading-primary">
+          A healthy meal delivered to your door, every single day
+        </h1>
+        <p class="hero-description">
+          The smart 365-days-per-year food subscription that will make you eat
+          healthy again. Tailored to your personal tastes and nutritional needs.
+          We have delivered 250,000+ meals last year!
+        </p>
+        <!-- add router-link -->
+        <a href="#" class="btn">Start eating well</a>
+        <a href="#" class="btn">Learn more &darr;</a>
+      </div>
+      <!-- image container -->
+      <div class="hero-img-box">
+        <img
+          :src="imageBox"
+          alt="Woman enjoying food, meals in storage container, and food bolws on a table"
+          class="hero-img"
+        />
+      </div>
     </div>
   </section>
 </template>
@@ -27,3 +36,21 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.hero-img {
+  /* Image container is 100% filled */
+  width: 100%;
+}
+
+.hero {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  max-width: 130rem;
+  margin: 0 auto;
+}
+
+.heading-primary {
+  font-size: 6.2rem;
+}
+</style>
